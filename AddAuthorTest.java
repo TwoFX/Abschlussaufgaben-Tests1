a7d0d86f50d69795526caadd93fc7a129dbfa1d1
Markus Himmel <markus@himmel-villmar.de>
2017-02-13 18:33:23 +0100

    Add tests, refine Terminal class

---
+++ AddAuthorTest.java
@@ -0,0 +1,163 @@
+package edu.kit.informatik.bibliography.tests;
+
+import edu.kit.informatik.bibliography.Console;
+import edu.kit.informatik.bibliography.TrackedEntity;
+import edu.kit.informatik.Terminal;
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.Before;
+import java.util.*;
+
+public class AddAuthorTest
+{
+    @Before
+    public void setUp()
+    {
+        TrackedEntity.reset();
+    }
+
+    @Test
+    public void AddAuthorInvalidArgumentCountTest()
+    {
+
+        String[] commands = new String[]
+        {
+            "add author",
+            "add author ",
+            "add author , ",
+            "add author A",
+            "add author ,",
+            "add author A,",
+            "add author ,A",
+            "add author ,A,B",
+            "add author A,,B",
+            "add author A,B,",
+            "add author A B",
+            "add author,A,B",
+            "add authorA,B",
+            "add author A,B,C",
+            "add author ,A,B,C",
+            "add author A,B ,C",
+            "add author A,B,C,D",
+            "quit"
+        };
+
+        Terminal.initialize(commands);
+
+        Console.main(null);
+
+        ArrayList<Terminal.Output> outp = new ArrayList<>(
+            Collections.nCopies(commands.length - 1, Terminal.error));
+        outp.add(Terminal.ok);
+
+        assertArrayEquals(outp.toArray(), Terminal.getOutput());
+        Terminal.finish();
+    }
+
+    @Test
+    public void AddAuthorInvalidArgumentFormatTest()
+    {
+
+        String[] commands = new String[]
+        {
+            "add author A a,B",
+            "add author A,B b",
+            "add author A ,b",
+            "add author A,b ",
+            "add author A, b",
+            "add author D.,Wagner",
+            "add author Titus,O'Neil",
+            "add author a1,B",
+            "add author A,b1",
+            "add author 345,A",
+            "add author B,123",
+            "add author A&B,C",
+            "add author C,B^t",
+            "add author 123,456",
+            "add author Professor,Mueller-Quade",
+            "add author Professor,Müller",
+            "add author Marku5,Himmel",
+            "add author Markus,Himme1",
+            "quit"
+        };
+
+        Terminal.initialize(commands);
+
+        Console.main(null);
+
+        ArrayList<Terminal.Output> outp = new ArrayList<>(
+            Collections.nCopies(commands.length - 1, Terminal.error));
+        outp.add(Terminal.ok);
+
+        assertArrayEquals(outp.toArray(), Terminal.getOutput());
+        Terminal.finish();
+    }
+
+    @Test
+    public void AddAuthorValidArgumentFormatTest()
+    {
+
+        String[] commands = new String[]
+        {
+            "add author Markus,Himmel",
+            "add author markus,himmel",
+            "add author m,h",
+            "add author marku,himme",
+            "add author Himmel,Markus",
+            "add author Markus,Markus",
+            "add author Himmel,Himmel",
+            "add author Lukas,Himmel",
+            "add author Lukas,Graham",
+            "add author L,Himmel",
+            "add author LUKAS,HIMMEL",
+            "add author lUKAS,hIMMEL",
+            "add author LuKaS,HiMmEl",
+            "add author aaaaaaaaaaa,bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
+            "add author null,null",
+            "add author null,himmel",
+            "add author null,nil",
+            "add author nil,nil",
+            "add author NaN,NaN",
+            "quit"
+        };
+
+        Terminal.initialize(commands);
+
+        Console.main(null);
+
+        ArrayList<Terminal.Output> outp = new ArrayList<>(
+            Collections.nCopies(commands.length, Terminal.ok));
+
+        assertArrayEquals(outp.toArray(), Terminal.getOutput());
+        Terminal.finish();
+    }
+
+    @Test
+    public void AddAuthorTwiceTest()
+    {
+        Terminal.initialize(
+            "add author Markus,Himmel",
+            "add author Markus,HimmeL",
+            "add author Markus,Himmel",
+            "add author Markus,HimmEl",
+            "add author Markus,HimmeL",
+            "add author Markus,HimMel",
+            "add author Markus Himmel",
+            "quit");
+
+        Console.main(null);
+
+        assertArrayEquals(new Terminal.Output[] {
+            Terminal.ok,
+            Terminal.ok,
+            Terminal.error,
+            Terminal.ok,
+            Terminal.error,
+            Terminal.ok,
+            Terminal.error,
+            Terminal.ok }, Terminal.getOutput());
+        Terminal.finish();
+    }
+}
+
+// vim: set expandtab:
--- CompleteTest.java
+++ CompleteTest.java
@@ -1,13 +1,20 @@
 package edu.kit.informatik.bibliography.tests;
 
 import edu.kit.informatik.bibliography.Console;
+import edu.kit.informatik.bibliography.TrackedEntity;
 import edu.kit.informatik.Terminal;
 import static org.junit.Assert.*;
 import org.junit.Test;
+import org.junit.Before;
 import java.util.*;
 
 public class CompleteTest
 {
+    @Before
+    public void setUp()
+    {
+        TrackedEntity.reset();
+    }
 
     @Test
     public void immediatelyQuitTest()
@@ -16,7 +23,101 @@ public class CompleteTest
 
         Console.main(null);
 
-        assertArrayEquals(new Terminal.Output[] { new Terminal.StringOutput("Ok") }, Terminal.getOutput());
+        assertArrayEquals(new Terminal.Output[] { Terminal.ok }, Terminal.getOutput());
+        Terminal.finish();
+    }
+
+    @Test
+    public void barfOnInvalidCommands1Test()
+    {
+        // This test contains strings that should not match to any matcher,
+        // inputs which should match but have the wrong argument format
+        // are tested elsewhere.
+        String[] commands = new String[]
+        {
+            "qui",
+            "quitt",
+            "add Eniola,Lowry",
+            "add ICSA",
+            "author Eniola,Lowry",
+            "addd author En,Lo",
+            "add authr En,Lo",
+            "add-author En,Lo",
+            "add_author En,Lo",
+            "addauthor En,Lo",
+            "add  author En,Lo",
+            "addd journal TSE,IEEE",
+            "add jornal TSE,IEEE",
+            "add-journal TSE,IEEE",
+            "add_journal TSE,IEEE",
+            "addjournal TSE,IEEE",
+            "add  journal TSE,IEEE",
+            "journal TSE,IEEE",
+            "addd conference series ICSA",
+            "add conference ICSA",
+            "add series ICSA",
+            "add confrence series ICSA",
+            "add conference serie ICSA",
+            "add  conference series ICSA",
+            "add conference  series ICSA",
+            "add-conference-series ICSA",
+            "add conference-series ICSA",
+            "add conferenceseries ICSA",
+            "add_conference_series ICSA",
+            "add conference_series ICSA",
+            "addconferenceseries ICSA",
+            "conference series ICSA",
+            "series ICSA",
+            "add series conference ICSA",
+            "add confrence ICSA,2017,Gothenburg",
+            "add-conference ICSA,2017,Gothenburg",
+            "add_conference ICSA,2017,Gothenburg",
+            "addconference ICSA,2017,Gothenburg",
+            "add article ICSA:rr2017,2017,A B",
+            "add article conference ICSA:rr2017,2017,A B",
+            "add article to confrence ICSA:rr2017,2017,A B",
+            "add article to jornal ICSA:rr2-17,2017,A B",
+            "quit"
+        };
+
+        Terminal.initialize(commands);
+
+        Console.main(null);
+
+        ArrayList<Terminal.Output> outp = new ArrayList<>(
+            Collections.nCopies(commands.length - 1, Terminal.error));
+        outp.add(Terminal.ok);
+
+        assertArrayEquals(outp.toArray(), Terminal.getOutput());
+        Terminal.finish();
+
+    }
+
+    @Test
+    public void barfOnInvalidCommands2Test()
+    {
+        String[] commands = new String[]
+        {
+            "sdfkodsjfk",
+            "sdkfjdsklfjdkslafjdklsajdsklfjlkdsghjiuoerngkroaesbgnioesjgvsdngaiojerwisgfaies",
+            "a",
+            "b",
+            "",
+            " ",
+            "	",
+            "\u0000",
+            "quit"
+        };
+
+        Terminal.initialize(commands);
+
+        Console.main(null);
+
+        ArrayList<Terminal.Output> outp = new ArrayList<>(
+            Collections.nCopies(commands.length - 1, Terminal.error));
+        outp.add(Terminal.ok);
+
+        assertArrayEquals(outp.toArray(), Terminal.getOutput());
         Terminal.finish();
     }
 
@@ -28,9 +129,9 @@ public class CompleteTest
         Console.main(null);
 
         assertArrayEquals(new Terminal.Output[] {
-            new Terminal.StringOutput("Ok"),
+            Terminal.ok,
             Terminal.error,
-            new Terminal.StringOutput("Ok") }, Terminal.getOutput());
+            Terminal.ok }, Terminal.getOutput());
         Terminal.finish();
     }
 }
